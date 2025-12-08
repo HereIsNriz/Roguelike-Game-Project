764b310c4e4ccf70ab6325c059124c8a85e3d39d
Nriz <here.is.nriz@gmail.com>
2025-12-08 21:49:45 +0700

    Keep Player in bound
    - Completed make the player can not leave the boundary

--- Roguelike-Game-Project/Assets/Scripts/PlayerController.cs
+++ Roguelike-Game-Project/Assets/Scripts/PlayerController.cs
@@ -8,23 +8,40 @@ public class PlayerController : MonoBehaviour
 
     private float m_verticalInput;
     private float m_horizontalInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float m_boundary = 15f;
 
     // Update is called once per frame
     void Update()
     {
         m_verticalInput = Input.GetAxis("Vertical");
         m_horizontalInput = Input.GetAxis("Horizontal");
+
+        KeepPlayerInBound();
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(m_horizontalInput, 0, m_verticalInput).normalized;
         transform.Translate(movement * m_speed * Time.deltaTime);
+    }
+
+    // ABSTRACTION
+    private void KeepPlayerInBound()
+    {
+        if (transform.position.x <= -m_boundary)
+        {
+            transform.position = new Vector3(-m_boundary, transform.position.y, transform.position.z);
+        }else if (transform.position.x >= m_boundary)
+        {
+            transform.position = new Vector3(m_boundary, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z <= -m_boundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -m_boundary);
+        }else if (transform.position.z >= m_boundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, m_boundary);
+        }
     }
 }
